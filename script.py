cc970053afef735dd0e62b22cbb8573256019fff
mihaiolteanu <mihai.olteanu@fastmail.fm>
2015-03-15 10:17:17 +0200

    Implement read only new emails based on UID

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 
 - imaplib documentation
@@ -6,9 +7,11 @@ https://docs.python.org/3.4/library/imaplib.html
 http://tools.ietf.org/html/rfc3501
 """
 
+import sys
 import imaplib
 import password
 from email.parser import HeaderParser
+import time
 
 
 # List of email clients
@@ -24,6 +27,9 @@ PORT = "993"
 # Your email address.
 USER_NAME = "mihai_olteanu@fastmail.fm"
 # Set the password in the password.py.
+
+# Check for new email frequency, in seconds
+REFRESH_RATE = 3
 
 
 def get_folder_names(mail):
@@ -45,36 +51,27 @@ def get_folder_names(mail):
         if not len(entry) == 2:
             continue
         name = entry[1]
-        names.append(name)
+        names.append(name.strip())
 
     return names
 
 
-def get_unseen_emails_id(mail, folder):
+def get_uids(mail, folder, from_uid = 0):
     """
-    Return a list of ids for the unseen emails in the specified folder.
+    Return a list of unique ids for the current folder.
+    @param from_uid: only return uids greater than this
     """
-    # Asume the name of the folder is correct, the search function will throw 
-    # an error if the folder is not found.
     if not select_folder(mail, folder):
         return []
 
     try:
-        # 6.4.4. SEARCH Command
-        result, response = mail.search(None, 'UNSEEN')
+        # https://blog.jtlebi.fr/2013/04/12/fetching-all-messages-since-last-check-with-python-imap/
+        result, response = mail.uid('search', None, 'UID {}:*'.format(from_uid))
     except imaplib.IMAP4.error as err:
         print(err)
         return []
 
-    # The response is of the form [b'23 24 25..'], 
-    # where 23,24, 25 are the ids we're after
-    try:
-        response = response[0].decode().split()
-    except (IndexError, UnicodeError) as err:
-        print(err)
-        return[]
-
-    return response
+    return response[0].decode().split()
 
 
 def select_folder(mail, folder):
@@ -82,14 +79,19 @@ def select_folder(mail, folder):
     Make the given folder the active folder.
     All future actions are to be carried out relative to this folder.
     """
-    result, response = mail.select(folder)
+    try:
+        result, response = mail.select(folder)
+    except imaplib.IMAP4.error as err:
+        print(err)
+        print(folder)
+        return False
     # If no such folder exists
     if result == "NO":
         return False
     return True
 
 
-def get_email_header(mail, number, folder):
+def get_email_header(mail, folder, uid):
     """
     Returns the header of the email based on the message sequence number.
     The numbering starts from 1 through the number of emails in the folder.
@@ -99,7 +101,7 @@ def get_email_header(mail, number, folder):
 
     try:
         # 6.4.5. FETCH Command
-        result, response = mail.fetch(number, '(BODY[HEADER])')
+        result, response = mail.uid('fetch', uid, '(BODY[HEADER])')
     except imaplib.IMAP4.error as err:
         print(err)
         return []
@@ -122,16 +124,59 @@ def parse_email_header(header):
     return {'from': msg['From'], 'subject': msg['Subject']}
 
 
-mail_instance = imaplib.IMAP4_SSL(host=MAIL_CLIENT, port=PORT)
-mail_instance.login(user=USER_NAME, password=PASSWORD)
+##### Main program #####
+
+# Login
+mail = imaplib.IMAP4_SSL(MAIL_CLIENT, PORT)
+mail.login(USER_NAME, password.PASSWORD)
+
+# Always set to the greatest uid for each email folder.
+# If new emails arrive after we've set this, their ids will be greater than this.
+# UIDs are only unique for the selected folder and NOT for the whole account,
+# so keep the largest uid for each folder
+largest_uids = {}
     
-#folders = get_folder_names(mail_instance)
+# All the folders (or mailboxes) in the current email account.
+folders = get_folder_names(mail)
 
-folder = "INBOX"
+# Initialize.
+for folder in folders:
+    uids = get_uids(mail, folder)
+    try:
+        uid = uids[-1]
+    except IndexError:
+        # No email in this folder.
+        uid = 0
+    largest_uids[folder] = uid
+
+while(True):
+    time.sleep(REFRESH_RATE)
+    for folder in folders:
+        last_uid = largest_uids[folder]
+        uids = get_uids(mail, folder, from_uid=last_uid)
+        # The uids returned include the from_uid specified in the above call,
+        # if there are any emails in the specified folder, of course.
+        if (not uids) or (len(uids) == 1 and last_uid == uids[0]):
+            sys.stdout.write("moving on\n")
+            sys.stdout.flush()
+            # No mail or no new mail in this folder.
+            continue
+        for uid in uids[1:]:
+            header = get_email_header(mail, folder, uid)
+            info = parse_email_header(header)
+            sys.stdout.write(info['from'])
+            sys.stdout.flush()
+        # Update the local uids for future queries.
+        largest_uids[folder] = uids[-1]
 
 
-unseen_ids = get_unseen_emails_id(mail, folder)
-print(unseen_ids)
+#folder = "INBOX"
+#uids = get_uids(mail, folder, 5200)
+#print(uids)
+
+#for f in largest_uid.keys():
+#    print(f + ": " + str(largest_uid[f]))
+
 #email_response = get_email_header(mail, unseen_ids[0], folder)
 #info = parse_email_header(email_response)
 #print(info)
